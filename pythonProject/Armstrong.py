64a92fee738ea50b39f83d53d9f46076ab71ea3a
deepikakm321 <deepikakm321@gmail.com>
2025-01-07 14:38:58 +0530

    aexw

--- pythonProject/Armstrong.py
+++ pythonProject/Armstrong.py
@@ -8,6 +8,6 @@ while temp > 0:
     sum_of_powers += digit ** num_digits
     temp //= 10
 if sum_of_powers == n:
-    print(f"{n} is an Armstrong number")
+    print(f"{n} is an Armstrong number  ")
 else:
     print(f"{n} is not an Armstrong number")
